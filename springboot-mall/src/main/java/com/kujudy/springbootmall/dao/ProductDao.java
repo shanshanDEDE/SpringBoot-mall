0e84894f79ae99272767792789b29852dec03761
willy <willyjen1111@gmail.com>
2024-02-16 20:26:09 +0800

    [商品功能 - 十座新增商品]

--- springboot-mall/src/main/java/com/kujudy/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/kujudy/springbootmall/dao/ProductDao.java
@@ -1,9 +1,10 @@
 package com.kujudy.springbootmall.dao;
 
+import com.kujudy.springbootmall.dto.ProductRequest;
 import com.kujudy.springbootmall.model.Product;
 
 public interface ProductDao {
     Product getProductById(Integer productId);
-    
 
+    Integer createProduct(ProductRequest productRequest);
 }

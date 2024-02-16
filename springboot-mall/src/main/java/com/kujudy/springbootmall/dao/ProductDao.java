0245d10a01a17c57e573a42569948a33f28b9a04
willy <willyjen1111@gmail.com>
2024-02-16 15:12:35 +0800

    商品功能 - 實作查詢商品

---
+++ springboot-mall/src/main/java/com/kujudy/springbootmall/dao/ProductDao.java
@@ -0,0 +1,9 @@
+package com.kujudy.springbootmall.dao;
+
+import com.kujudy.springbootmall.model.Product;
+
+public interface ProductDao {
+    Product getProductById(Integer productId);
+    
+
+}
---
+++ springboot-mall/src/main/java/com/kujudy/springbootmall/service/ProductService.java
@@ -0,0 +1,9 @@
+package com.kujudy.springbootmall.service;
+
+import com.kujudy.springbootmall.model.Product;
+
+public interface ProductService {
+
+    Product getProductById(Integer productId);
+
+}

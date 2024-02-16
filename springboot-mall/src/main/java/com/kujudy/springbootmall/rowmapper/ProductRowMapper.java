7e6c105b1d505d6c0e14f92309d71ff36a786c7d
willy <willyjen1111@gmail.com>
2024-02-16 16:00:45 +0800

    商品功能 - 實作查詢商品

--- springboot-mall/src/main/java/com/kujudy/springbootmall/rowmapper/ProductRowMapper.java
+++ springboot-mall/src/main/java/com/kujudy/springbootmall/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.kujudy.springbootmall.rowmapper;
 
+import com.kujudy.springbootmall.constant.ProductCategory;
 import com.kujudy.springbootmall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -13,7 +14,14 @@ public class ProductRowMapper implements RowMapper<Product> {
 
         product.setProductId(rs.getInt("product_Id"));
         product.setProductName(rs.getString("product_name"));
-        product.setCategory(rs.getString("category"));
+
+
+        String categoryStr = rs.getString("category");
+        ProductCategory category = ProductCategory.valueOf(categoryStr);
+        product.setCategory(category);
+
+//        product.setCategory(ProductCategory.valueOf(rs.getString("category")));
+
         product.setImageUrl(rs.getString("image_url"));
         product.setPrice(rs.getInt("price"));
         product.setStock(rs.getInt("stock"));

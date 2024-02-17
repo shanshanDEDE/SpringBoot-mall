03913600d83b67c97ee19945529b68310b10cecb
willy <willyjen1111@gmail.com>
2024-02-17 19:38:58 +0800

    帳號功能 - 檢查註冊的 email,影藏返回的密碼

--- springboot-mall/src/main/java/com/kujudy/springbootmall/dto/UserRegisterRequest.java
+++ springboot-mall/src/main/java/com/kujudy/springbootmall/dto/UserRegisterRequest.java
@@ -1,10 +1,12 @@
 package com.kujudy.springbootmall.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public class UserRegisterRequest {
 
     @NotBlank
+    @Email
     private String email;
 
     @NotBlank

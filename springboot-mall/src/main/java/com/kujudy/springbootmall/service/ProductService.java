6e175dbff715154f8c203bc43f014396ff2a8b16
willy <willyjen1111@gmail.com>
2024-02-17 11:15:18 +0800

    [商品功能 - 實作查詢商品列表 (基礎實作)]

--- springboot-mall/src/main/java/com/kujudy/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/kujudy/springbootmall/service/ProductService.java
@@ -3,7 +3,12 @@ package com.kujudy.springbootmall.service;
 import com.kujudy.springbootmall.dto.ProductRequest;
 import com.kujudy.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
@@ -11,4 +16,5 @@ public interface ProductService {
     void updateProduct(Integer productId,ProductRequest productRequest);
 
     void deleteProductById(Integer productId);
+
 }
